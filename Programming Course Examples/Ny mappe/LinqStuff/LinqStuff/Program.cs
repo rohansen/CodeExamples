40f9f06b97115b6ca7049bc5a074dfa3b218236d
Ronni Hansen <hansen.ronni@gmail.com>
2016-08-04 13:15:54 +0200

    no idea

---
+++ Programming Course Examples/Ny mappe/LinqStuff/LinqStuff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> func = (a, b) => { return (a * a) + (b * b); };
+
+
+            var agg = Enumerable.Range(1,300).Select(x=>x.ToString()).Aggregate((accumulated,next)=>accumulated + ", " + next);
+            Console.WriteLine(func(2, 2));
+            Console.WriteLine("Aggregate\n" + agg);
+
+        }
+    }
+}
