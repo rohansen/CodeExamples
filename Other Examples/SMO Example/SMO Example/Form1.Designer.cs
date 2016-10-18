5aa634597d678982fd12c06bcfa2cdab16dc489a
Ronni Hansen <hansen.ronni@gmail.com>
2016-10-18 08:17:59 +0200

    minor modification

--- Other Examples/SMO Example/SMO Example/Form1.Designer.cs
+++ Other Examples/SMO Example/SMO Example/Form1.Designer.cs
@@ -66,9 +66,10 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(266, 344);
+            this.tabControl1.Size = new System.Drawing.Size(355, 423);
             this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
             this.tabControl1.TabIndex = 4;
             // 
@@ -78,10 +79,11 @@
             this.tabPage1.Controls.Add(this.label5);
             this.tabPage1.Controls.Add(this.button3);
             this.tabPage1.Controls.Add(this.listBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(258, 318);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(347, 394);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Create";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -90,9 +92,10 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(9, 285);
+            this.linkLabel1.Location = new System.Drawing.Point(12, 351);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(60, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(77, 17);
             this.linkLabel1.TabIndex = 9;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Go to UMS";
@@ -101,17 +104,19 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(5, 12);
+            this.label5.Location = new System.Drawing.Point(7, 15);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(238, 13);
+            this.label5.Size = new System.Drawing.Size(317, 17);
             this.label5.TabIndex = 8;
             this.label5.Text = "Students - drag in UMS CSV File to load students";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(8, 259);
+            this.button3.Location = new System.Drawing.Point(11, 319);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(235, 23);
+            this.button3.Size = new System.Drawing.Size(313, 28);
             this.button3.TabIndex = 7;
             this.button3.Text = "Create Database for selected student(s)";
             this.button3.UseVisualStyleBackColor = true;
@@ -120,11 +125,13 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(8, 28);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(11, 34);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
             this.listBox1.ScrollAlwaysVisible = true;
             this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.listBox1.Size = new System.Drawing.Size(235, 225);
+            this.listBox1.Size = new System.Drawing.Size(312, 276);
             this.listBox1.TabIndex = 6;
             // 
             // tabPage3
@@ -134,9 +141,10 @@
             this.tabPage3.Controls.Add(this.listBox2);
             this.tabPage3.Controls.Add(this.button1);
             this.tabPage3.Controls.Add(this.txtSearch);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(258, 318);
+            this.tabPage3.Size = new System.Drawing.Size(347, 394);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Delete";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -144,35 +152,40 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(8, 10);
+            this.label7.Location = new System.Drawing.Point(11, 12);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(218, 13);
+            this.label7.Size = new System.Drawing.Size(293, 17);
             this.label7.TabIndex = 4;
             this.label7.Text = "Delete one or more databases and the logins";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(5, 48);
+            this.label6.Location = new System.Drawing.Point(7, 59);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.Size = new System.Drawing.Size(128, 17);
             this.label6.TabIndex = 3;
             this.label6.Text = "Enter search string";
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(8, 90);
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(11, 111);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox2.Name = "listBox2";
             this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.listBox2.Size = new System.Drawing.Size(242, 95);
+            this.listBox2.Size = new System.Drawing.Size(321, 116);
             this.listBox2.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 191);
+            this.button1.Location = new System.Drawing.Point(11, 235);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Remove";
             this.button1.UseVisualStyleBackColor = true;
@@ -180,9 +193,10 @@
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(8, 64);
+            this.txtSearch.Location = new System.Drawing.Point(11, 79);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(242, 20);
+            this.txtSearch.Size = new System.Drawing.Size(321, 22);
             this.txtSearch.TabIndex = 0;
             this.txtSearch.TextChanged += new System.EventHandler(this.findDatabases);
             this.txtSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.findDatabases);
@@ -196,10 +210,11 @@
             this.tabPage2.Controls.Add(this.txtSqlInstanceName);
             this.tabPage2.Controls.Add(this.txtSqlPassword);
             this.tabPage2.Controls.Add(this.txtSqlUserName);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(258, 318);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(347, 394);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Settings";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -207,71 +222,79 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 15);
+            this.label4.Location = new System.Drawing.Point(15, 18);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.Size = new System.Drawing.Size(185, 17);
             this.label4.TabIndex = 6;
             this.label4.Text = "SQL Connection Information";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 104);
+            this.label3.Location = new System.Drawing.Point(15, 128);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.Size = new System.Drawing.Size(36, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "URL";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 78);
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Password";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 52);
+            this.label1.Location = new System.Drawing.Point(11, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(73, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Username";
             // 
             // txtSqlInstanceName
             // 
-            this.txtSqlInstanceName.Location = new System.Drawing.Point(70, 101);
+            this.txtSqlInstanceName.Location = new System.Drawing.Point(93, 124);
+            this.txtSqlInstanceName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSqlInstanceName.Name = "txtSqlInstanceName";
-            this.txtSqlInstanceName.Size = new System.Drawing.Size(150, 20);
+            this.txtSqlInstanceName.Size = new System.Drawing.Size(199, 22);
             this.txtSqlInstanceName.TabIndex = 2;
             this.txtSqlInstanceName.Text = "localhost\\SQLExpress";
             // 
             // txtSqlPassword
             // 
-            this.txtSqlPassword.Location = new System.Drawing.Point(70, 75);
+            this.txtSqlPassword.Location = new System.Drawing.Point(93, 92);
+            this.txtSqlPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSqlPassword.Name = "txtSqlPassword";
-            this.txtSqlPassword.Size = new System.Drawing.Size(150, 20);
+            this.txtSqlPassword.Size = new System.Drawing.Size(199, 22);
             this.txtSqlPassword.TabIndex = 1;
-            this.txtSqlPassword.Text = "rONNI48651571";
+            this.txtSqlPassword.Text = "1234";
             // 
             // txtSqlUserName
             // 
-            this.txtSqlUserName.Location = new System.Drawing.Point(70, 49);
+            this.txtSqlUserName.Location = new System.Drawing.Point(93, 60);
+            this.txtSqlUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSqlUserName.Name = "txtSqlUserName";
-            this.txtSqlUserName.Size = new System.Drawing.Size(150, 20);
+            this.txtSqlUserName.Size = new System.Drawing.Size(199, 22);
             this.txtSqlUserName.TabIndex = 0;
             this.txtSqlUserName.Text = "sa";
             // 
             // Form1
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
-            this.ClientSize = new System.Drawing.Size(266, 344);
+            this.ClientSize = new System.Drawing.Size(355, 423);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "UCN SimpleDB";
             this.Load += new System.EventHandler(this.Form1_Load);
